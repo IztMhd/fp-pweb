e8b086ade6596b7344f06e8dbfdcfb9f79b1a40f
labib3013 <79435333+labib3013@users.noreply.github.com>
2022-12-08 22:43:52 +0700

    add : views and routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,16 +2,28 @@ import { createRouter, createWebHistory } from "vue-router";
 import Playlist from "../views/MyPlaylist.vue";
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Playlist,
-  },
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: Playlist,
+  // },
   {
     path: "/edit/:id",
     name: "edit",
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/EditPlaylist.vue"),
+  },
+  {
+    path: "/",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/MainPlaylist.vue"),
+  },
+  {
+    path: "/playlist",
+    name: "playlist",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/DetailPlaylist.vue"),
   },
 ];
 
